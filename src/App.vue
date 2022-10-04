158447c9d64484ea7db95689916c66218f986da1
Scrapie-pie <neontec2@gmail.com>
2022-10-04 13:38:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <EventsCalendar></EventsCalendar>
+  </div>
+</template>
+
+<script>
+import EventsCalendar from './components/EventsCalendar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EventsCalendar
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/style';
+#app {
+  padding: 35px 0;
+  overflow: hidden;
+}
+</style>
